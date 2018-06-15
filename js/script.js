963079206732663f23367028b09a9cfa42f3e3be
alegerieoran <settibelkacem313@gmail.com>
2018-06-15 17:00:48 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// le dom est la representation(l'interpritation) de la page html par javascript
+//modulo dévise toujours le premier par le deuxieme
+
+// // Initialisation de l'objet moi variable global
+// var moi;
+
+// moi = "setti";//une valeur de type string  //entre les "" c'est type text
+
+// moi=30; //type number
+
+// moi=true; //Boolean // comparesant entre true et false
+
+// moi=1.05; //type float(float= nombre avec vergule)
+
+// moi= ["setti", 30,12.5]; //type Array  // un tableau avec plusieures valeurs
+
+// // alert("salut "+ moi);
+
+// console.log("salut "+ moi[0] +" tu as " +moi[1]+" et " +moi[2] );
+
+// document.write(moi)//la fonction de document sert a "interargir" modifier le dom
+
+var a = 1;//a est un nom variable(arbitraire) // 1 est une valeur (arbitraire)
+var b = 1;
+console.log(a + b);// le console ne s'execute seulement dans la console
+
+var fille = "ines";
+
+fille = "brune";
+
+fille = 9;
+
+fille=["ines","brune",9];
+
+console.log("Ma fille "+fille[0]+" est "+fille[1]+" elle a "+fille[2]+" ans.");
+
+
+var a = "la formation";
+    a = "Développeur web";
+    a = 10;
+    a =["la formation", "développeur web",10];
+    console.log("je fait partie de "+a[0]+" "+a[1]+" qui va durée "+a[2]+" mois");
+
+
+
+
+
+
